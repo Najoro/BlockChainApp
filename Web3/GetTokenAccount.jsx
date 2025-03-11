1aa05480736bccf3e43496158df19af31a950b52
Najoro Fanantenana <najofanantenana@gmail.com>
2025-03-11 09:26:27 +0100

    drowpdown display token

--- Web3/GetTokenAccount.jsx
+++ Web3/GetTokenAccount.jsx
@@ -1,31 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { AccountLayout, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token';
-import { PublicKey } from '@solana/web3.js';
-import { View, TouchableOpacity, ActivityIndicator } from 'react-native';
-import ImageTextCard from '@/navigation/screens/HomeContent/ImageTextCard';
+import React, { useEffect, useState } from "react";
+import { AccountLayout, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import { PublicKey } from "@solana/web3.js";
+import { View, TouchableOpacity, ActivityIndicator } from "react-native";
+import ImageTextCard from "@/navigation/screens/HomeContent/ImageTextCard";
 import { useNavigation } from "@react-navigation/native";
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import { Picker } from "@react-native-picker/picker";
+import {SOLANA_WALLET_PUBLIC_KEY, SOLANA_RPC_URL} from "@/app.config";
+import {Connection,clusterApiUrl} from "@solana/web3.js";
+
+const cn = new Connection(SOLANA_RPC_URL);
+const pk = SOLANA_WALLET_PUBLIC_KEY;
+
 import { Buffer } from "buffer";
 
 global.Buffer = Buffer;
+// Adresse du programme Metaplex Token Metadata
+const METADATA_PROGRAM_ID = new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
 
-const fetchTokens = async (connection, publicKey) => {
+const fetchTokens = async (connection=cn, publicKey=pk) => {
   try {
-    const tokenAccounts = await connection.getTokenAccountsByOwner(new PublicKey(publicKey), {
-      programId: TOKEN_2022_PROGRAM_ID,
-    });
+    const tokenAccounts = await connection.getTokenAccountsByOwner(
+      new PublicKey(publicKey),
+      {
+        programId: TOKEN_2022_PROGRAM_ID,
+      }
+    );
 
     return await Promise.all(
       tokenAccounts.value.map(async (tokenAccount) => {
         const accountData = AccountLayout.decode(tokenAccount.account.data);
         const mintAddress = new PublicKey(accountData.mint).toBase58();
         const amount = parseInt(accountData.amount) / 10 ** 9;
-
+        
+        const metadata = await getTokenMetadata(connection, mintAddress);
         return {
           mintAddress,
           amount,
           name: "Ariary",
           symbol: "Ar",
-          image: "https://red-leading-marmot-607.mypinata.cloud/ipfs/bafybeih7laba3limk6h6qq2u7gky4xcmqqvfbyx6atq2uillvxil2azaxy",
+          image:
+            "https://red-leading-marmot-607.mypinata.cloud/ipfs/bafybeih7laba3limk6h6qq2u7gky4xcmqqvfbyx6atq2uillvxil2azaxy",
         };
       })
     );
@@ -35,7 +50,36 @@ const fetchTokens = async (connection, publicKey) => {
   }
 };
 
-const GetTokenAccount = ({ connection, publicKey }) => {
+//get metadata
+async function getTokenMetadata(connection,tokenMintAddress) {
+  try {
+    const mintPublicKey = new PublicKey(tokenMintAddress);
+
+    // Trouver l'adresse des métadonnées associées au token
+    const [metadataPDA] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("metadata"),
+        METADATA_PROGRAM_ID.toBuffer(),
+        mintPublicKey.toBuffer(),
+      ],
+      METADATA_PROGRAM_ID
+    );
+    console.log("metadataPDA:", metadataPDA.toBase58());
+
+    // Récupérer les informations du compte
+    const accountInfo = await connection.getAccountInfo(metadataPDA);
+    if (accountInfo) {
+      const metadata = Metadata.deserialize(accountInfo.data)[0];
+      console.log("Metadata :", metadata);
+    } else {
+      console.log("Aucune metadata trouvée pour ce token.",  metadataPDA.toBase58());
+    }
+  } catch (error) {
+    console.error("Erreur lors de la récupération des métadonnées :", error);
+  }
+}
+
+const GetTokenAccount = ({ connection=cn, publicKey=pk }) => {
   const [tokens, setTokens] = useState([]);
 
   useEffect(() => {
@@ -48,9 +92,10 @@ const GetTokenAccount = ({ connection, publicKey }) => {
   return tokens;
 };
 
-const TokenDisplay = ({ connection, publicKey }) => {
+
+const TokenDisplay = ({ connection=cn, publicKey=pk }) => {
   const navigation = useNavigation();
-  const tokens = GetTokenAccount({ connection, publicKey });
+  const tokens = GetTokenAccount({ connection, publicKey })
 
   return (
     <View>
@@ -58,7 +103,12 @@ const TokenDisplay = ({ connection, publicKey }) => {
         <ActivityIndicator size="large" color="#007bff" />
       ) : (
         tokens.map((token, index) => (
-          <TouchableOpacity key={index} onPress={() => navigation.navigate("Envoyer", { token: token.mintAddress })}>
+          <TouchableOpacity
+            key={index}
+            onPress={() =>
+              navigation.navigate("Envoyer", { token: token.mintAddress })
+            }
+          >
             <ImageTextCard
               imageSource={{ uri: token.image }}
               title={token.name}
@@ -71,4 +121,24 @@ const TokenDisplay = ({ connection, publicKey }) => {
   );
 };
 
-export { GetTokenAccount, TokenDisplay };
+const TokendropDownDisplay = ({ connection=cn, publicKey=pk }) =>{
+  const tokens = GetTokenAccount({connection,publicKey});
+  const [selectedValue, setSelectedValue] = useState('');
+
+  return(
+    <View>
+      {tokens.length === 0 ? (
+        <ActivityIndicator size="large" color="#007bff" />
+      ) : (
+        <Picker selectedValue={selectedValue} onValueChange={(itemValue) => setSelectedValue(itemValue)}>
+         {tokens.map((token, index) => (
+          <Picker.Item key={index} label={token.mintAddress} value={token.mintAddress} />
+        ))}
+      </Picker>
+      )}
+    </View>
+  );
+}
+
+
+export { GetTokenAccount, TokenDisplay,TokendropDownDisplay };
--- Web3/TokensList.jsx
+++ Web3/TokensList.jsx
@@ -4,22 +4,20 @@ import 'react-native-get-random-values';
 import { v4 as uuidv4 } from "uuid";
 import {Connection,clusterApiUrl} from "@solana/web3.js";
 import GetSolanaBalance from './GetSolanaBalance';
-import {TokenDisplay} from "./GetTokenAccount";
-
-import {SOLANA_WALLET_PUBLIC_KEY, SOLANA_RPC_URL} from "@/app.config";
+import {TokenDisplay,TokendropDownDisplay} from "./GetTokenAccount";
 
 import { Buffer } from "buffer";
 global.Buffer = Buffer;
 
-const connection = new Connection(SOLANA_RPC_URL);
-const pubkey = SOLANA_WALLET_PUBLIC_KEY;
+// const connection = new Connection(SOLANA_RPC_URL);
+// const pubkey = SOLANA_WALLET_PUBLIC_KEY;
 
 const TokensList = () => {
   return (
     <View>
       <View>
         <Text style={styles.title}>Token associé</Text>
-        <TokenDisplay connection={connection} publicKey={pubkey}  />
+        <TokenDisplay/>
       </View>
     </View>
   )
